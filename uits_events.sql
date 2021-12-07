da4098092d16182ab3f59e29855e8c8d8c92c371
ksnirob <ksnirob@gmail.com>
2021-12-07 21:23:58 +0600

    connect to db

---
+++ uits_events.sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2021 at 04:18 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uits_events`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` int(11) NOT NULL,
+  `event_title` varchar(50) NOT NULL,
+  `event_price` int(11) DEFAULT NULL,
+  `participents` int(100) DEFAULT 0,
+  `img_link` text DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`event_id`, `event_title`, `event_price`, `participents`, `img_link`, `type_id`) VALUES
+(23, 'Programming Contest', 0, 0, 'coding.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_info`
+--
+
+CREATE TABLE `event_info` (
+  `event_id` int(10) NOT NULL,
+  `Date` date DEFAULT NULL,
+  `time` varchar(20) NOT NULL,
+  `location` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `event_info`
+--
+
+INSERT INTO `event_info` (`event_id`, `Date`, `time`, `location`) VALUES
+(23, '2021-12-28', '12:00 am', '401 Lab');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_type`
+--
+
+CREATE TABLE `event_type` (
+  `type_id` int(10) NOT NULL,
+  `type_title` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `event_type`
+--
+
+INSERT INTO `event_type` (`type_id`, `type_title`) VALUES
+(1, 'Technical Events'),
+(2, 'Gaming Events'),
+(3, 'On Stage Events'),
+(4, 'Off Stage Events');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participent`
+--
+
+CREATE TABLE `participent` (
+  `stuid` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `branch` varchar(11) NOT NULL,
+  `sem` int(11) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `dept` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registered`
+--
+
+CREATE TABLE `registered` (
+  `id` int(11) NOT NULL,
+  `stuid` varchar(20) DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `registered`
+--
+DELIMITER $$
+CREATE TRIGGER `count` AFTER INSERT ON `registered` FOR EACH ROW update events
+set events.participents=events.participents+1
+WHERE events.event_id=new.event_id
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff_coordinator`
+--
+
+CREATE TABLE `staff_coordinator` (
+  `stid` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff_coordinator`
+--
+
+INSERT INTO `staff_coordinator` (`stid`, `name`, `phone`, `event_id`) VALUES
+(23, 'Bathen', NULL, 23);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_coordinator`
+--
+
+CREATE TABLE `student_coordinator` (
+  `sid` int(11) NOT NULL,
+  `st_name` varchar(100) NOT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_coordinator`
+--
+
+INSERT INTO `student_coordinator` (`sid`, `st_name`, `phone`, `event_id`) VALUES
+(23, 'Khaled', '0183068107', 23);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `event_info`
+--
+ALTER TABLE `event_info`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `event_type`
+--
+ALTER TABLE `event_type`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- Indexes for table `participent`
+--
+ALTER TABLE `participent`
+  ADD PRIMARY KEY (`stuid`);
+
+--
+-- Indexes for table `registered`
+--
+ALTER TABLE `registered`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `staff_coordinator`
+--
+ALTER TABLE `staff_coordinator`
+  ADD PRIMARY KEY (`stid`);
+
+--
+-- Indexes for table `student_coordinator`
+--
+ALTER TABLE `student_coordinator`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `event_info`
+--
+ALTER TABLE `event_info`
+  MODIFY `event_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=183;
+
+--
+-- AUTO_INCREMENT for table `registered`
+--
+ALTER TABLE `registered`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `staff_coordinator`
+--
+ALTER TABLE `staff_coordinator`
+  MODIFY `stid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=182;
+
+--
+-- AUTO_INCREMENT for table `student_coordinator`
+--
+ALTER TABLE `student_coordinator`
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=182;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
